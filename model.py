c9d3eb0195ae0d3a00e87091294290117217a099
Mohamed Aziz Ayari <139351844+Asembris@users.noreply.github.com>
2026-08-13 12:12:31 +0200

    Step 51: sample_next_token

--- model.py
+++ model.py
@@ -418,8 +418,11 @@ def normalize_counts_to_probs(n_matrix):
     res=n_matrix/sums
     return res
 
-# Step 51 - sample_next_token (not yet solved)
-# TODO: implement
+# Step 51 - sample_next_token
+def sample_next_token(p_matrix, current_id, rng):
+    """Sample the next token id from P[current_id] using rng."""
+    # TODO: draw one categorical sample from the row of p_matrix at current_id
+    return rng.choice(len(p_matrix[current_id]), p=p_matrix[current_id])
 
 # Step 52 - generate_sequence (not yet solved)
 # TODO: implement
