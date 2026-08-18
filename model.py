d0e0293d0834795a6701f24a555145414f6eb9df
Mohamed Aziz Ayari <139351844+Asembris@users.noreply.github.com>
2026-08-18 19:26:33 +0200

    Step 86: layernorm_forward_normalize

--- model.py
+++ model.py
@@ -741,8 +741,14 @@ def layernorm_forward_variance(x, mean):
     res = np.mean((x - mean) ** 2, axis=-1, keepdims=True)
     return res
 
-# Step 86 - layernorm_forward_normalize (not yet solved)
-# TODO: implement
+# Step 86 - layernorm_forward_normalize
+import numpy as np
+
+def layernorm_forward_normalize(x, mean, var, eps):
+    """Normalize each row of x to zero mean and unit variance."""
+    # TODO: subtract the per-row mean and divide by sqrt(var + eps)
+    res=(x-mean)/np.sqrt(var+eps)
+    return res
 
 # Step 87 - layernorm_forward_affine (not yet solved)
 # TODO: implement
